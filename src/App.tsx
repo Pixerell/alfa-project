eb7a1c0e61348cba3b9596a168ff242b89b4302c
Pixerell <alexmegaflexer@gmail.com>
2025-11-02 15:38:22 +0300

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useAppDispatch } from "./store/store";
 import { fetchProducts } from "./api/fetchProducts";
 import ProductsPage from "./ui/pages/ProductsPage/ProductsPage";
